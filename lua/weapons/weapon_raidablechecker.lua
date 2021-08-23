89a6306170959325caa7b2da613ceb64d51f7c20
NarrowGlint <31360427+NarrowGlint@users.noreply.github.com>
2021-08-22 21:12:45 -0400

    Create weapon_raidablechecker.lua

---
+++ lua/weapons/weapon_raidablechecker.lua
@@ -0,0 +1,99 @@
+--NarrowGlints Raidable Checker
+
+SWEP.PrintName = "Raidables Checker"
+SWEP.Author = "NarrowGlint"
+SWEP.Category = "NarrowGlint"
+SWEP.Instructions = "Left mouse to check for printers, meth, etc."
+
+SWEP.Spawnable = true
+
+SWEP.Primary.ClipSize		= -1
+SWEP.Primary.DefaultClip	= -1
+SWEP.Primary.Automatic		= false
+SWEP.Primary.Ammo		    = "none"
+
+SWEP.Secondary.ClipSize		= -1
+SWEP.Secondary.DefaultClip	= -1
+SWEP.Secondary.Automatic	= false
+SWEP.Secondary.Ammo		    = "none"
+
+SWEP.Weight			        = 5
+SWEP.AutoSwitchTo		    = false
+SWEP.AutoSwitchFrom		    = false
+
+SWEP.Slot                   = 2
+SWEP.SlotPos			    = 2
+SWEP.DrawAmmo			    = false
+SWEP.DrawCrosshair		    = true
+
+
+local foundMessage = " was found within the range!"
+
+local failMessage = "No more raidables were found within the range!"
+
+
+function SWEP:RaidableCheck()
+	
+	local owner = self:GetOwner()
+
+	if (not owner:IsValid()) then return end
+
+	if (CLIENT) then return end
+
+	local playerPos = owner:GetPos()
+
+-- This adds the front square pos (up and to the right)
+	
+	local frontPos = Vector(0,0,0)
+	
+	frontPos:Add(playerPos + Vector(800, -900, -250))
+
+-- This adds the behind square pos (back and to the left)
+	
+	local backPos = Vector(0,0,0)
+
+	backPos:Add(playerPos + Vector(-800, 900, 250))
+	
+-- Create the box for the entities
+
+	local boxEnts = ents.FindInBox(frontPos, backPos)
+
+-- Im new to Lua, sorry there is probably a better way to do this.
+
+	for k,v in ipairs(boxEnts) do
+
+		local className = v:GetClass()
+
+		if string.StartWith(className, "sprinter_") then
+			
+			owner:ChatPrint(className .. foundMessage)
+		
+		elseif string.StartWith(className, "zmlab2") then
+
+			if className == "zmlab2_npc" or className == "zmlab2_equipment" then
+
+				do_nothing = true
+			
+			else
+
+				owner:ChatPrint(className .. foundMessage) 
+			end
+			
+		else
+			no_found_entities = true
+		end
+
+	end
+
+	if no_found_entities == true then
+		owner:ChatPrint(failMessage)
+	end
+end
+
+-- This is more fun
+
+function SWEP:PrimaryAttack()
+
+	self:RaidableCheck()
+
+end
