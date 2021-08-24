3fb978b9219a67cde88bdf2fb36d3c97a3581279
NarrowGlint <31360427+NarrowGlint@users.noreply.github.com>
2021-08-24 12:08:39 -0400

    Create config.lua

---
+++ lua/autorun/config.lua
@@ -0,0 +1,9 @@
+-- Put your raidable items here ---- Items that you want to return to the players chat if they are found.
+
+-- Make sure to add them as this format
+
+
+local raidables = {
+	"item_ammo_357",
+	"item_suit",
+}
