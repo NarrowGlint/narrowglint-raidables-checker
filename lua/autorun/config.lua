f4d6c145207fa4ef9923c0b36c8daeea9b1eeb6f
NarrowGlint <31360427+NarrowGlint@users.noreply.github.com>
2021-08-24 12:44:37 -0400

    these are the defaults I would use for my server for sPrinters and zeros meth 2

--- lua/autorun/config.lua
+++ lua/autorun/config.lua
@@ -4,6 +4,24 @@
 
 
 raidables = {
-	"item_ammo_357",
-	"item_suit",
+	"sprinter_bronze_printer",
+	"sprinter_silver_printer",
+	"sprinter_gold_printer",
+	"sprinter_vip_printer",
+	"sprinter_professional_printer",
+	"sprinter_rack",
+	"zmlab2_item_autobreaker",
+	"zmlab2_item_crate",
+	"zmlab2_dropoff",
+	"zmlab2_machine_filler",
+	"zmlab2_machine_filter",
+	"zmlab2_machine_frezzer",
+	"zmlab2_item_frezzertray",
+	"zmlab2_machine_furnace",
+	"zmlab2_item_meth",
+	"zmlab2_machine_mixer",
+	"zmlab2_item_palette",
+	"zmlab2_storage",
+	"zmlab2_table",
+	"zmlab2_machine_ventilation"
 }
